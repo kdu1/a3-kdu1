36523386ef6dc3691d379ba07cff43ab7e18a42b
Glitch (hello-express) <none>
2022-09-16 21:57:08 +0000

    🚟🙈 Checkpoint ./server.js:47973311/1533

--- server.js
+++ server.js
@@ -20,3 +20,55 @@ app.get("/", function(request, response) {
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://kdu1:a3Password123@cluster0.zjyldku.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+const appdata = [
+]
+
+//gets soonest dated task that has high priority
+/*const getMin = function(array){
+    let date = 10000;
+    let indexPrev = [];
+    for(let i = 0; i < array.length; i++){
+        curDate = parseInt(array[i].dueDate)
+        curPrior = array[i].priority
+        if(curDate < date){
+            if(curPrior === "high"){
+                if(indexPrev.length > 0){
+                    for(let j = 0; j < indexPrev.length; j++){
+                        array[indexPrev[j]].urgent = 0
+                    }
+                    indexPrev = []
+                }
+                date = curDate
+                indexPrev.push(i)
+            }
+        }
+        else if(curDate === date){
+            if(curPrior === "high"){
+                indexPrev.push(i)
+            }
+        }
+        console.log("indexPrev")
+        console.log(indexPrev)
+    }
+    console.log("date")
+    console.log(date)
+    return date
+}
+
+const remove = function(array){
+    const index = 0
+    if(index > -1){
+        array.splice(index, 1)
+    }
+    console.log(array)
+}*/
