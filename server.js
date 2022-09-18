99538d13ace7ff0b00269fd18fcb8c7e137854da
Glitch (hello-express) <none>
2022-09-18 20:51:52 +0000

    🌌💾 Checkpoint ./server.js:47973311/545

--- server.js
+++ server.js
@@ -25,6 +25,22 @@ app.get("/", function(request, response) {
 });*/
 
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser')
+
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+// POST /login gets urlencoded bodies
+app.post('/login', urlencodedParser, function (req, res) {
+  res.send('welcome, ' + req.body.username)
+})
+
+// POST /api/users gets JSON bodies
+app.post('/api/users', jsonParser, function (req, res) {
+  // create user in req.body
+})
 
 
 // listen for requests :)
