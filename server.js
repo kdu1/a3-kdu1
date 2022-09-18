512b59489b161c7c3c1aa01ec11afcab32b18baf
Glitch (hello-express) <none>
2022-09-18 20:39:42 +0000

    🍗🐧 Checkpoint ./server.js:47973311/60

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(cookieParser());
 
 app.get("/", function(request, response) {
   console.log(request.cookies);
+  console.log('Signed Cookies: ', request.signedCookies);
   response.sendFile(__dirname + "/views/login.html");
 });
 
