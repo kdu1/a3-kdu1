a6f7ce3764d3dd46888ace411a9572d68956b463
Glitch (hello-express) <none>
2022-09-19 16:20:01 +0000

    🐋🚛 Checkpoint ./loginclient.js:47973311/167 ./views/login.html:47973311/5

--- loginclient.js
+++ loginclient.js
@@ -1,3 +1,10 @@
+
+
 const loginForm = document.forms[0];
 const username = loginForm.elements["username"];
 const password = loginForm.elements["password"];
+
+loginForm.onsubmit = function(event) {
+  //push as key value pair
+  console.log (username, password);
+}
